cb8431296c51fee6d233624bc193c3e05eb7f33d
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-11-29 16:42:49 +0100

    DRY

--- private/functions.php
+++ private/functions.php
@@ -5,9 +5,11 @@ function hashPass($pass){
     return $newPass;
 }
 
-function createUser($name, $email, $phone, $pass){
     $conn = new PDO('sqlite:/home/emmanuel/flowerly.db');
     $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+function createUser($name, $email, $phone, $pass){
+    global $conn;
     $stmt = $conn -> prepare("INSERT INTO users (name, phone, email, pass) VALUES (:name, :phone, :email, :pass)");
     $stmt -> bindParam(':name', $name);
     $stmt -> bindParam(':phone', $phone);
@@ -18,8 +20,7 @@ function createUser($name, $email, $phone, $pass){
 
 function FindUser($param, $string){
     //this functi0n id f0r the c0mm0n users to validate if a user is already in the database
-    $conn = new PDO('sqlite:/home/emmanuel/flowerly.db');
-    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    global $conn;
     $stmt = $conn -> prepare("SELECT * FROM users WHERE $param = :string");
     $stmt -> bindParam(':string', $string, PDO::PARAM_STR);
     $stmt -> execute();
@@ -30,8 +31,7 @@ function FindUser($param, $string){
 
 function getUser($param, $string){
     //this functi0n id f0r the c0mm0n users to validate if a user is already in the database
-    $conn = new PDO('sqlite:/home/emmanuel/flowerly.db');
-    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    global $conn;
     $stmt = $conn -> prepare("SELECT * FROM users WHERE $param = :string");
     $stmt -> bindParam(':string', $string, PDO::PARAM_STR);
     $stmt -> execute();
@@ -41,9 +41,8 @@ function getUser($param, $string){
 }
 
 function updateUser($newName, $newEmail, $newPhone, $newPass, $param, $string){
-    try{   
-        $conn = new PDO('sqlite:/home/emmanuel/flowerly.db');
-        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try{
+        global $conn;
         $stmt = $conn -> prepare("UPDATE users SET name = :newName, phone = :newPhone,email = :newEmail, pass = :newPass WHERE $param = :string");
         $stmt -> bindparam(':newName', $newName);
         $stmt -> bindparam(':newPhone', $newPhone);
@@ -62,8 +61,7 @@ function updateUser($newName, $newEmail, $newPhone, $newPass, $param, $string){
 
 function checkUser($email, $pass){
     // to check if password and email match eachother
-    $conn = new PDO('sqlite:/home/emmanuel/flowerly.db');
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    global $conn;
     $stmt = $conn->prepare("SELECT pass FROM users WHERE email = :Email");
     $stmt->bindParam(':Email', $email, PDO::PARAM_STR);
     $stmt->execute();
@@ -75,8 +73,7 @@ function checkUser($email, $pass){
 }
 
 function fetchAllProducts(){
-    $conn = new PDO('sqlite:/home/emmanuel/flowerly.db'); 
-        $stmt = $conn -> prepare("SELECT * FROM posts");
+        global $conn;
         $stmt -> execute();
         while($row  = $stmt -> fetch(PDO::FETCH_ASSOC)){
             $result[] = $row;
@@ -85,8 +82,7 @@ function fetchAllProducts(){
 }
 
 function createOrder($username, $productname, $price){
-    $conn = new PDO('sqlite:/home/emmanuel/flowerly.db');
-    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    global $conn;
     $stmt = $conn -> prepare("INSERT INTO orders (username, productname, price) VALUES (:username, :productname, :price)");
     $stmt -> bindParam(':username', $username);
     $stmt -> bindParam(':productname', $productname);
